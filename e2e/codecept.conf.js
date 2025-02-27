f02001174bf895f4cd7233cc9575e8feb7a5b29e
saenishide <nishide.even@gmail.com>
2025-02-27 22:30:41 +0900

    URLを環境変数から渡すようにした

--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -1,4 +1,6 @@
 const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+require('dotenv').config();
+
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
@@ -12,7 +14,7 @@ exports.config = {
   output: './output',
   helpers: {
     Playwright: {
-      url: 'http://localhost',
+      url: process.env.BASE_URL,
       show: true,
       browser: 'chromium'
     }
@@ -22,5 +24,16 @@ exports.config = {
   },
   translation: 'en-US',
   vocabularies: ['./vocabularies.json'],
-  name: 'e2e'
+  name: 'e2e',
+  plugins: {
+    allure: {
+      enabled: true,
+      require: "allure-codeceptjs",
+    },
+    stepByStepReport: {
+      enabled: true,
+      screenshotsForAllureReport: true,
+      deleteSuccessful: false,
+    }
+  }
 }
--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,7 +1,7 @@
 SuiteOf('スモークテスト');
 
 Scenario('Webサイトを開きログインする', ({ I }) => {
-    I.amOnPage('https://e2e-test-sample-production.up.railway.app/')
+    I.amOnPage('/')
     I.click('ログインする')
     I.fillField('ユーザー名', 'user1')
     I.fillField('パスワード', 'super-strong-passphrase')
